0a5d122cf94d3ff67befd3696036a1921753c9f8
umairx97 <umair.bajwa@hotmail.com>
2019-03-06 23:40:11 +0500

    6 - March - 2019

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React,{Component} from "react";
 
 const App = () => {
-  return <div>{Umair("Bio amla")}</div>;
+  return (
+    <div>
+      <Umair name="asdas" id="2101" Profession="developer" />
+    </div>
+  );
 };
 
 export default App;
 
-const Umair = Product => {
-  if (Product === "Sunsilk") {
-    let a = "2";
-    let b = "Pakistan";
-
-    return `Id is: ${a} And Manufacturer is ${b}`;
-  } else {
-    return "No Product Found";
+class Umair extends Component {
+  render() {
+    return (
+      <h1>
+        Hello my name is {this.props.name} my id is {this.props.id} and my profession is{" "}
+        {this.props.Profession}
+        {" "}
+      </h1>
+    );
   }
-};
+}
+
+const Umair = (props) => {
+  return (
+    <h1>
+      Hello my name is {props.name} my id is {props.id} and my profession is {props.Profession}
+    </h1>
+  )
+}
