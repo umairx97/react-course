b9c4bfc42fd2364173378b912e6559404e75e873
umairx97 <umair.bajwa@hotmail.com>
2019-03-06 22:02:57 +0500

    Added Functions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const App = () => { 
-  return (
-    <div>
-      Hello
-    </div>
-  )
-}
+const App = () => {
+  return <div>{Umair("Bio amla")}</div>;
+};
 
 export default App;
+
+const Umair = Product => {
+  if (Product === "Sunsilk") {
+    let a = "2";
+    let b = "Pakistan";
+
+    return `Id is: ${a} And Manufacturer is ${b}`;
+  } else {
+    return "No Product Found";
+  }
+};
