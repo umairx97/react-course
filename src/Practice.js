48340b950b6e6e6cccec4d2673cc0995d2275084
umairx97 <umair.bajwa@hotmail.com>
2019-03-12 22:22:06 +0500

    Studied JS

--- src/Practice.js
+++ src/Practice.js
@@ -1,12 +1,58 @@
 import React from "react";
+import "./mycard.css";
 
+const Users = [
+  {
+    id: 1,
+    imageUrl: "/images/team2.jpg",
+    name: "Umair",
+    designation: "developer",
+    University: "Indus University"
+  },
+  {
+    id: 2,
+    imageUrl: "/images/team2.jpg",
+    name: "Asad",
+    designation: "developer",
+    University: "Indus University"
+  },
+  {
+    id: 3,
+    imageUrl: "/images/team2.jpg",
+    name: "Shahbaz",
+    designation: "developer",
+    University: "Indus University"
+  }
+];
 
-const Practice = () => { 
-    return ( 
-        <div> 
-        hello
+const Practice = () => {
+  return (
+    <div>
+      {Users.map(user => (
+        <div className="card">
+          <img src={user.imageUrl} alt="John" style={{ width: "100%" }} />
+          <h1>{user.name}</h1>
+          <p class="title">{user.designation}</p>
+          <p>{user.University}</p>
+          <a href="#">
+            <i class="fa fa-dribbble" />
+          </a>
+          <a href="#">
+            <i class="fa fa-twitter" />
+          </a>
+          <a href="#">
+            <i class="fa fa-linkedin" />
+          </a>
+          <a href="#">
+            <i class="fa fa-facebook" />
+          </a>
+          <p>
+            <button>Contact</button>
+          </p>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
 export default Practice;
---
+++ src/prac.js
@@ -0,0 +1,44 @@
+// const Car = {
+//     model: 2019,
+//     brand: 'Nissan',
+//     manufacturer: 'Nissan Motors',
+//     wheels: {
+//         type: 'racer'
+//     }
+// }
+
+// const {brand, manufacturer, model} = Car
+
+// console.log(brand, manufacturer,model)
+
+// console.log(Car.brand, Car.manufacturer, Car.model, Car.wheels)
+
+// const Users = [1, 'umair', 2, 'Ashir']
+
+// const Users = [
+//     {
+//         name: 'umair',
+//         id: 1
+//     },
+//     {
+//         name: 'asad',
+//         id: 2
+//     }
+// ]
+
+// console.log('Users', Users)
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
+  .catch(err => console.log(err, 'Cannot complete'))
+
+
+// const promise = new Promise((res, rej) => {
+//   setInterval(() => {
+//     res();
+//   }, 5000);
+// });
+
+// promise.then(() => console.log('finished'))
+
