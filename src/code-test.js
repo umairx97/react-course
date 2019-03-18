b19a1c918e8125a01a6d0e3770f96d542387cf80
umairx97 <umair.bajwa@hotmail.com>
2019-03-18 23:46:52 +0500

    added Test

---
+++ src/code-test.js
@@ -0,0 +1,88 @@
+// *****************************************************************************************************
+// ******************************************************************************************************
+/* TASK NUMBER 1: 
+    
+Write a code that prints your name in the console by taking the name as an argument to a method
+*/
+
+// *****************************************************************************************************
+// ******************************************************************************************************
+/* TASK NUMBER 2 
+
+Write an arrow function to print your name in a single line of code 
+*/
+
+// *****************************************************************************************************
+// ******************************************************************************************************
+/* TASK NUMBER 3 
+
+Write an arrow function that takes two arguments as numbers and returns the sum of those two numbers.
+(The arrow function should be in a single line)
+*/
+
+// *****************************************************************************************************
+// ******************************************************************************************************
+/* TASK NUMBER 4
+
+Write an arrow function that takes two numbers as parameters and checks if they are equal.
+if the condition returns true the console should print "equal" and if the condition is false the
+console should write not "equal" 
+*/
+
+// *****************************************************************************************************
+// ******************************************************************************************************
+/* TASK NUMBER 5
+
+Refactor the code written in TASK 4 and do the same thing but by using ternary operator and in a single line
+*/
+
+// *****************************************************************************************************
+// ******************************************************************************************************
+/* TASK NUMBER 6
+
+Create an array of names and add a name at the end of the array by using array methods also add name 
+at the start of an array 
+*/
+
+// *****************************************************************************************************
+// ******************************************************************************************************
+/* TASK NUMBER 7
+
+let arr = ['Umair', 'Asad']; 
+
+Take the array above and print the length of the array to the console
+*/
+
+// *****************************************************************************************************
+// ******************************************************************************************************
+/* TASK NUMBER 8
+
+let obj = { 
+    name: 'Ashir', 
+    profession: 'Developer', 
+    id: '1'
+ }
+
+ Take the above object and print a sentence using template strings as below 
+
+ (My name is Ashir and my profession is Developer)
+*/
+
+// *****************************************************************************************************
+// ******************************************************************************************************
+/* TASK NUMBER 9
+Create an array of student names and pass it in a function. the function should be an arrow function 
+and the functions duty is to check the length of an array and print "too many students" if array length 
+is greater than 5. and should print "Add more students" if length is less than 5 
+
+Do the above task with the use of ternary operator
+*/
+
+// *****************************************************************************************************
+// ******************************************************************************************************
+/* TASK NUMBER 10
+
+Write an arrow function that takes two strings as an argument and checks if those strings are equal 
+
+If they are equal the console should print "equal" and if not the console should print "not equal"
+*/
