079895b6fd9a5302bc9ed84f9d809a1c5b7fe7c6
umairx97 <umair.bajwa@hotmail.com>
2019-03-19 21:18:09 +0500

    Completed Tasks

--- src/code-test.js
+++ src/code-test.js
@@ -5,12 +5,21 @@
 Write a code that prints your name in the console by taking the name as an argument to a method
 */
 
+// const getName = (name) => {
+//     console.log(name)
+// }
+
+// getName('Umair')
+
 // *****************************************************************************************************
 // ******************************************************************************************************
 /* TASK NUMBER 2 
 
 Write an arrow function to print your name in a single line of code 
 */
+
+// const getName = name => console.log(name);
+// getName('Ashir')
 
 // *****************************************************************************************************
 // ******************************************************************************************************
@@ -19,6 +28,11 @@ Write an arrow function to print your name in a single line of code
 Write an arrow function that takes two arguments as numbers and returns the sum of those two numbers.
 (The arrow function should be in a single line)
 */
+
+// const addSum = (a,b) => {
+//     return a + b
+// }
+// console.log(addSum(5,6))
 
 // *****************************************************************************************************
 // ******************************************************************************************************
@@ -29,12 +43,27 @@ if the condition returns true the console should print "equal" and if the condit
 console should write not "equal" 
 */
 
+// const addSum = (a,b) => {
+//     if(a === b){
+//         console.log('Equal')
+//     } else {
+//         console.log('Not equal')
+//     }
+// }
+
+// addSum(11, 11);
+
 // *****************************************************************************************************
 // ******************************************************************************************************
 /* TASK NUMBER 5
 
 Refactor the code written in TASK 4 and do the same thing but by using ternary operator and in a single line
 */
+
+// const a = 10;
+// const b = 20;
+
+// a === b ? console.log('Equal') : console.log('Not equal')
 
 // *****************************************************************************************************
 // ******************************************************************************************************
@@ -43,6 +72,13 @@ Refactor the code written in TASK 4 and do the same thing but by using ternary o
 Create an array of names and add a name at the end of the array by using array methods also add name 
 at the start of an array 
 */
+
+// const arr = ['Umair', 'Asad']
+
+// arr.push('Ashir')
+// arr.unshift('Shahbaz')
+
+// console.log(arr)
 
 // *****************************************************************************************************
 // ******************************************************************************************************
@@ -53,20 +89,28 @@ let arr = ['Umair', 'Asad'];
 Take the array above and print the length of the array to the console
 */
 
+// const arr = ['Umair', 'Asad']
+
+// console.log(arr.length)
+
 // *****************************************************************************************************
 // ******************************************************************************************************
 /* TASK NUMBER 8
 
-let obj = { 
-    name: 'Ashir', 
-    profession: 'Developer', 
-    id: '1'
- }
+
 
  Take the above object and print a sentence using template strings as below 
 
  (My name is Ashir and my profession is Developer)
 */
+
+// let obj = {
+//     name: 'Ashir',
+//     profession: 'Developer',
+//     id: '1'
+// }
+
+// console.log(`My name is ${obj.name} My profession is ${obj.profession} `)
 
 // *****************************************************************************************************
 // ******************************************************************************************************
@@ -78,6 +122,15 @@ is greater than 5. and should print "Add more students" if length is less than 5
 Do the above task with the use of ternary operator
 */
 
+// const students = ["Umair", "Asad", "Shahbaz", "Ashir", "Hamza", "Moomal"];
+
+// const getStudents = students => {
+//   students.length > 5
+//     ? console.log("Too many students")
+//     : console.log("Add more students");
+// };
+
+// getStudents(students);
 // *****************************************************************************************************
 // ******************************************************************************************************
 /* TASK NUMBER 10
@@ -86,3 +139,9 @@ Write an arrow function that takes two strings as an argument and checks if thos
 
 If they are equal the console should print "equal" and if not the console should print "not equal"
 */
+
+// const getStrings = (a, b) => {
+//   a === b ? console.log("Equal") : console.log("Not equal");
+// };
+
+// getStrings('Umair', 'abcde')
