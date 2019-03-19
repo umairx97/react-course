6a99bbbaaf5eed0bec710347dbf1ea10cd3c2ce8
umairx97 <umair.bajwa@hotmail.com>
2019-03-20 01:24:46 +0500

    20-March test

---
+++ src/code-test-2.js
@@ -0,0 +1,127 @@
+// *****************************************************************************************************
+// ******************************************************************************************************
+/* TASK NUMBER 1: 
+ 
+Write an arrow function that takes an array as an argument.
+The function's duty is to delete the last element from the array and return it
+
+*/
+
+// ******************************************************************************************************
+/* TASK NUMBER 2: 
+ 
+Write an arrow function that takes an array as an argument.
+The function's duty is add an element at the start of an array and return it 
+
+*/
+
+
+// ******************************************************************************************************
+/* TASK NUMBER 3: 
+ 
+Write an arrow function that takes an argument.
+The function's duty is to check the arguments type and print the type by using switch case statements
+
+
+For example if i pass in a string the function should print string on the console
+if i pass a number the console should print number
+
+*/
+
+
+// ******************************************************************************************************
+/* TASK NUMBER 4: 
+ 
+Write an arrow function that takes an array of numbers as an argument and function's job is to multiply each element of an array by 2 and print it
+
+*/
+
+
+// ******************************************************************************************************
+/* TASK NUMBER 5: 
+ 
+Write an arrow function that takes an array as an argument.
+The function's duty is add an element at the start of an array and return it 
+
+*/
+
+// ******************************************************************************************************
+/* TASK NUMBER 6: 
+ 
+Write an arrow function that takes an argument as object.
+The functions duty is to write the output as follows by using template strings
+
+
+
+(My name is Ashir and i am a Developer)
+
+*/
+
+// ******************************************************************************************************
+/* TASK NUMBER 7: 
+ 
+const Obj = {
+    name: 'Umair',
+    Id: '2101', 
+    profession: 'React Developer'
+
+}
+
+write an arrow function that takes the above object as an argument and destructure out name and profession from it and print it on the console 
+
+Dont do it like this =>>>> Obj.name, Obj.profession
+
+(use destructuring)
+
+*/
+
+
+// ******************************************************************************************************
+/* TASK NUMBER 8: 
+ 
+write an arrow function that takes two arguments as strings
+The functions duty is to check if the first argument passed is a string or not and then print true or false on the console 
+
+
+The second duty of the function is to check the second argument whether it is a number or not  and then print true or false on the console 
+
+*/
+
+
+// ******************************************************************************************************
+/* TASK NUMBER 9: 
+ 
+Write an arrow function that takes an array as an argument.
+The function's duty is add an element at the start of an array and return it 
+
+*/
+
+
+// ******************************************************************************************************
+/* TASK NUMBER 9: 
+ 
+Write an arrow function that does not get any arguments but should return a random id number if called against a variable 
+
+Example 
+ const id = generateId();
+
+ the generate function should generate a random id between 1 - 1000 and return it 
+
+
+ HINT => Use javascript's own Math Library
+
+*/
+
+
+// ******************************************************************************************************
+/* TASK NUMBER 10: 
+ 
+Write an arrow function that takes in a string as an argument an the functions duty is to check the number of characters in a string a print it on the console
+*/
+
+
+
+
+
+
+
