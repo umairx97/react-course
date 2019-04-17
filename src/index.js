26fd9ce48d1519a341efaea6d9c50ae193abdab8
umairx97 <umair.bajwa@hotmail.com>
2019-04-17 19:34:10 +0500

    Login Page

--- src/index.js
+++ src/index.js
@@ -7,5 +7,76 @@ import ReactDOM from "react-dom";
 // import Practice from './Practice'
 // import Areeb from './Areeb';
 // import EighteenMarch from "./eighteen-march";
-import Conditional from "./Conditional";
-ReactDOM.render(<Conditional />, document.getElementById("root"));
+// import Conditional from "./Conditional";
+
+class App extends React.Component {
+  state = {
+    loggedin: false
+  };
+
+  handleLogin = () => {
+    this.setState({ loggedin: !this.state.loggedin });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.loggedin ? (
+          <Dashboard handleLogin={this.handleLogin} />
+        ) : (
+          <Login handleLogin={this.handleLogin} />
+        )}
+      </div>
+    );
+  }
+}
+
+class Login extends React.Component {
+  state = {
+    username: "",
+    password: ""
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.props.handleLogin}>
+          Username
+          <br />
+          <input
+            type="text"
+            name="username"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <br />
+          Password
+          <br />
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+          <br />
+          <button>Click me</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+const Dashboard = props => {
+  return (
+    <div>
+      Dashboard Page
+      <button onClick={props.handleLogin}> Log out</button>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
