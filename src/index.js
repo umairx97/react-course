a1f2e941768fb05ec79ff9366658981468a29806
umairx97 <umair.bajwa@hotmail.com>
2019-05-06 22:15:42 +0500

    Study Routes

--- src/index.js
+++ src/index.js
@@ -1,82 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import App from './App';
-// import Users from './Users';
-// import App2 from './App2';
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 
-// import Practice from './Practice'
-// import Areeb from './Areeb';
-// import EighteenMarch from "./eighteen-march";
-// import Conditional from "./Conditional";
+const Home = props => {
+  console.log(props);
+  return <div>This is a home page</div>;
+};
+
+const About = props => {
+  console.log(props);
+
+  return <div>This is a about page</div>;
+};
 
 class App extends React.Component {
-  state = {
-    loggedin: false
-  };
-
-  handleLogin = () => {
-    this.setState({ loggedin: !this.state.loggedin });
-  };
-
   render() {
     return (
       <div>
-        {this.state.loggedin ? (
-          <Dashboard handleLogin={this.handleLogin} />
-        ) : (
-          <Login handleLogin={this.handleLogin} />
-        )}
+        <BrowserRouter>
+          <Link to="/about">Goto about page</Link>
+          <Link to="/">Goto Home page</Link>
+          <Switch>
+            <Route path="/" exact render={() => <Home name="umair" />} />
+            <Route path="/about" exact component={About} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
 }
-
-class Login extends React.Component {
-  state = {
-    username: "",
-    password: ""
-  };
-
-  handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.props.handleLogin}>
-          Username
-          <br />
-          <input
-            type="text"
-            name="username"
-            value={this.state.username}
-            onChange={this.handleChange}
-          />
-          <br />
-          Password
-          <br />
-          <input
-            type="password"
-            name="password"
-            value={this.state.password}
-            onChange={this.handleChange}
-          />
-          <br />
-          <button>Click me</button>
-        </form>
-      </div>
-    );
-  }
-}
-
-const Dashboard = props => {
-  return (
-    <div>
-      Dashboard Page
-      <button onClick={props.handleLogin}> Log out</button>
-    </div>
-  );
-};
 
 ReactDOM.render(<App />, document.getElementById("root"));
